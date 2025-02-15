5ea04cae434fbfbe1ac4baf1ad68dbbb0167b2b8
PhucNHB <phucnhb519.dev@gmail.com>
2025-02-15 21:40:15 +0700

    fix: validator, isDeleted field, and value of Audience in DependenciesInjection

--- WTL_Clean_Architecture/src/Domain/Specifications/Users/GetListUsersSpecification.cs
+++ WTL_Clean_Architecture/src/Domain/Specifications/Users/GetListUsersSpecification.cs
@@ -5,7 +5,7 @@ namespace Domain.Specifications.Users
     public class GetListUsersSpecification : Specification<User, long>
     {
         public GetListUsersSpecification(int? pageNumber, int? pageSize, string? searchText, int? roleId) :
-            base(user => user.IsDeleted == true && (user.RoleId == roleId || roleId == null) &&
+            base(user => user.IsDeleted != true && (user.RoleId == roleId || roleId == null) &&
             (string.IsNullOrEmpty(searchText) || (user.FullName != null && user.FullName.Contains(searchText.Trim())) ||
             (user.PhoneNumber != null && user.PhoneNumber.Contains(searchText.Trim())) ||
             (user.Address != null && user.Address.Contains(searchText.Trim())) ||
